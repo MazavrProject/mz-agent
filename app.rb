2dc54bf161434fca1ce5a59694afd10ba11dd9d4
Sergei O. Udalov <sergei.udalov@gmail.com>
2018-06-06 14:31:21 +0300

    multiple hosts

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ logger = Logger.new STDOUT
 
 logger.info "Monitoring started!"
 
-system = System.new ENV['HOSTS']
+systems = ENV['HOSTS'].split(',').map { |host| System.new host }
 storage = Storage.new(
   host: ENV['CLICKHOUSE_HOST'], 
   user: ENV['CLICKHOUSE_USER'], 
@@ -16,11 +16,12 @@ storage = Storage.new(
 
 while true
 
+  systems.each do |system|
+    data =  { host: system.host, cpu_load: system.cpu_load.round(2), memory_usage: system.memory_usage.round(2) }
+    logger.info "#{data.to_json}"
 
-  data =  { host: system.host, cpu_load: system.cpu_load.round(2), memory_usage: system.memory_usage.round(2) }
-  logger.info "#{data.to_json}"
-
-  storage.write data
+    storage.write data
+  end
 
   `ls`
   sleep 1
